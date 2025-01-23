ed19527514c84fa329d4c4fa5b449700a1f830c1
omp10 <omparteki2002@gmail.com>
2025-01-23 23:21:19 +0530

    code generation

--- components/custom/CodeView.jsx
+++ components/custom/CodeView.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   SandpackProvider,
   SandpackLayout,
@@ -7,8 +7,36 @@ import {
   SandpackPreview,
   SandpackFileExplorer 
 } from "@codesandbox/sandpack-react";
+import Lookup from "@/data/Lookup";
+import axios from "axios";
+import { MessagesContext } from "@/context/MessagesContext";
+import Prompt from "@/data/Prompt";
 function CodeView() {
   const [activeTab,setActiveTab]=useState('code');
+  const [files,setFiles]=useState(Lookup?.DEFAULT_FILE);
+  const {messages,setMessages}=useContext(MessagesContext);
+
+  useEffect(()=>{
+    if(messages?.length>0){
+      const role=messages[messages?.length-1].role;
+      if(role=='user'){
+        GeneratedAiCode();
+      }
+    }
+
+  },[messages])
+
+  const GeneratedAiCode=async()=>{
+    const PROMPT=messages[messages?.length-1]?.content+" "+Prompt.CODE_GEN_PROMPT
+    const result=await axios.post('/api/gen-ai-code',{
+      prompt:PROMPT
+    });
+    console.log(result.data);
+    const aiResp=result.data;
+    const mergedFiles={...Lookup.DEFAULT_FILE,...aiResp?.files}
+    setFiles(mergedFiles);
+  }
+
   return (
     <div>
       <div className="bg-[#181818] w-full p-2 border">
@@ -17,7 +45,17 @@ function CodeView() {
         <h2 onClick={()=>setActiveTab('preview')} className={`text-sm cursor-pointer ${activeTab=='preview'&&'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full '}`}>Preview</h2>           
         </div>
       </div>
-      <SandpackProvider template="react" theme={"dark"}>
+      <SandpackProvider template="react" theme={"dark"}
+      files={files}
+      customSetup={{
+        dependencies:{
+          ...Lookup.DEPENDANCY
+        }
+      }}
+      options={{
+        externalResources:['https://unpkg.com/@tailwindcss/browser@4']
+      }}
+      >
         <SandpackLayout>
           {activeTab=='code'?<>
         <SandpackFileExplorer style={{height:'80vh'}}/>
--- configs/AiModel.jsx
+++ configs/AiModel.jsx
@@ -18,12 +18,25 @@ const {
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
   };
+  const CodeGenerationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "application/json",
+  };
   
    export const chatSession = model.startChat({
       generationConfig,
       history: [
       ],
     });
+
+    export const GenAiCode=model.startChat({
+      generationConfig:CodeGenerationConfig,
+      history:[]
+
+    })
   
     // const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
     // console.log(result.response.text());
