050b667527618d79f39c69c65160ac7152596e9b
omp10 <omparteki2002@gmail.com>
2025-01-24 00:14:15 +0530

    code generation 3.0

--- components/custom/CodeView.jsx
+++ components/custom/CodeView.jsx
@@ -11,10 +11,34 @@ import Lookup from "@/data/Lookup";
 import axios from "axios";
 import { MessagesContext } from "@/context/MessagesContext";
 import Prompt from "@/data/Prompt";
+import { useConvex, useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Loader2Icon } from "lucide-react";
 function CodeView() {
+  const {id}=useParams();
   const [activeTab,setActiveTab]=useState('code');
   const [files,setFiles]=useState(Lookup?.DEFAULT_FILE);
   const {messages,setMessages}=useContext(MessagesContext);
+  const UpdateFiles=useMutation(api.workspace.UpdateFiles);
+  const convex=useConvex();
+  const [loading,setLoading]=useState(false);
+
+  useEffect(()=>{
+    id&&GetFiles();
+
+  },[id])
+
+  const GetFiles=async()=>{
+    setLoading(true);
+    const result=await convex.query(api.workspace.GetWorkspace,{
+      workspaceId:id
+    });
+    const mergedFiles={...Lookup.DEFAULT_FILE,...result?.fileData}
+    setFiles(mergedFiles);
+    setLoading(false);
+    
+  }
 
   useEffect(()=>{
     if(messages?.length>0){
@@ -27,18 +51,27 @@ function CodeView() {
   },[messages])
 
   const GeneratedAiCode=async()=>{
+    setLoading(true);
     const PROMPT=JSON.stringify(messages)+" "+Prompt.CODE_GEN_PROMPT
     const result=await axios.post('/api/gen-ai-code',{
       prompt:PROMPT
     });
     console.log(result.data);
     const aiResp=result.data;
+
     const mergedFiles={...Lookup.DEFAULT_FILE,...aiResp?.files}
     setFiles(mergedFiles);
+    await UpdateFiles({
+      workspaceId:id,
+      files:aiResp?.files
+
+    });
+    setLoading(false);
+
   }
 
   return (
-    <div>
+    <div className="relative">
       <div className="bg-[#181818] w-full p-2 border">
         <div className="  flex items-center flex-wrap shrink-0 gap-3 bg-black p-1 w-[140px] justify-center rounded-full">
         <h2 onClick={()=>setActiveTab('code')} className={`text-sm cursor-pointer ${activeTab=='code'&&'text-blue-500 bg-blue-500 bg-opacity-25 p-1 px-2 rounded-full '}`}>Code</h2>           
@@ -66,6 +99,10 @@ function CodeView() {
           </>}
         </SandpackLayout>
       </SandpackProvider>
+      {loading&&<div className="p-10 bg-gray-900 opacity-80 absolute top-0 rounded-lg w-full h-full flex justify-center items-center">
+        <Loader2Icon className="animate-spin"/>
+        <h2>Generating Files</h2>
+      </div>}
     </div>
   );
 }
